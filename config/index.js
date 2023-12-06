9462df9c75128ead10f051e1b7564e20c461165e
Cristina RP <cristinarp95@gmail.com>
2023-12-06 10:27:56 +0100

    Add config

--- config/index.js
+++ config/index.js
@@ -35,4 +35,6 @@ module.exports = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use(cors());
+  app.use(express.static("public"));
 };
