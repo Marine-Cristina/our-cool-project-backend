4013b7cdea8879e98ccdc436043167bd8e1d452b
Cristina RP <cristinarp95@gmail.com>
2023-12-15 02:27:37 +0100

    Update auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -116,7 +116,7 @@ router.post("/login", (req, res, next) => {
         });
 
         // Send the token as the response
-        res.status(200).json({ authToken: authToken });
+        res.status(200).json({ user: payload, authToken: authToken });
       } else {
         res.status(401).json({ message: "Unable to authenticate the user" });
       }
